fe00fdede9faa6cbff4fed62e2940bde9c101d0c
Takahiro <qwerty@suishin.xyz>
2018-08-26 02:26:56 +0000

    imgUP5

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,8 +1,8 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   storage :file
-
-
+  include CarrierWave::MiniMagick
+  process :resize_to_limit => [300, 300]
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
